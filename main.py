9efff8ce7e299638ef4a641c29752e30511a849f
muradmurtuzaliev <mur.murtuzaliev@yandex.ru>
2021-02-08 00:42:31 +0300

    Added template of bot

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
+from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
+import vk_api
+from const import GROUP_ID, TOKEN
+
+vk = vk_api.VkApi(token=TOKEN)
+vk._auth_token()
+vk.get_api()
+longpoll = VkBotLongPoll(vk, GROUP_ID)
+
 if __name__ == "__main__":
-    pass
+    while True:
+        print("bot is online")
+        for event in longpoll.listen():
+            pass
