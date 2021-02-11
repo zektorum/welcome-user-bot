c9fc4c2c956a3ebe9155e983dac9f56b26f1a6db
muradmurtuzaliev <mur.murtuzaliev@yandex.ru>
2021-02-12 00:41:58 +0300

    Refactoring and other changes

--- main.py
+++ main.py
@@ -1,13 +1,32 @@
 from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
+from vk_api.longpoll import VkLongPoll
 import vk_api
-from const import GROUP_ID, TOKEN
+from const import GROUP_ID, GROUP_TOKEN, BOT_TOKEN
 
-vk = vk_api.VkApi(token=TOKEN)
-vk._auth_token()
-vk.get_api()
-longpoll = VkBotLongPoll(vk, GROUP_ID)
+# Авторизация токена бота для работы с пользователем-администратором
+vk_bot = vk_api.VkApi(token=BOT_TOKEN)
+vk_bot._auth_token()
+vk_bot.get_api()
+longpoll = VkBotLongPoll(vk_bot, GROUP_ID)
 
-if __name__ == "__main__":
+# Авторизация токена группы для рассылки сообщений пользователям
+# vk_group = vk_api.VkApi(token=GROUP_TOKEN)
+# vk_group._auth_token()
+# vk_group.get_api()
+# longpoll = VkLongPoll(vk_group)
+
+
+def get_list_of_repost_users():
+    """ Получение списка пользователей, репостнувших запись """
+    pass
+
+
+def send_message():
+    """ Отправка сообщений """
+    pass
+
+
+def main():
     while True:
         print("bot is online")
         try:
@@ -18,3 +37,7 @@ if __name__ == "__main__":
         except AttributeError as e:
             print("Ошибка! Получено событие из сообщений сообщества. Ожидалось из беседы")
             continue
+
+
+if __name__ == "__main__":
+    main()
