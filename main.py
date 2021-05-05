4d9da07b0e3d394d6456fab9b3751bc2563a90d8
muradmurtuzaliev <mur.murtuzaliev@yandex.ru>
2021-05-05 13:30:28 +0300

    Tempotary commit

--- main.py
+++ main.py
@@ -1,25 +1,41 @@
 from time import sleep
+from datetime import datetime
 
 from vk import bot_longpoll as longpoll
 from vk import NEW_MESSAGE_EVENT
-from tools import get_user_id, get_user_name_by_id, send_message
+from tools import *
 
 
-def main():
-    while True:
-        print("bot is online")
+def event_loop():
+    """ Цикл обработки событий. Прилашённым пользователям отправляется приветствие """
+    for event in longpoll.check():
         try:
-            for event in longpoll.listen():
-                if event.from_chat:
-                    if event.type == NEW_MESSAGE_EVENT:
-                        user_id = get_user_id(event.raw)
-                        if user_id:
-                            first_name = get_user_name_by_id(user_id)[0]
-                            send_message(event.chat_id, f"Welcome, [id{user_id}|{first_name}]!")
+            if event.from_chat:
+                if event.type == NEW_MESSAGE_EVENT:
+                    user_id = get_user_id(event.raw)
+                    if user_id:
+                        first_name = get_user_name_by_id(user_id)[0]
+                        send_message(event.chat_id, f"Welcome, [id{user_id}|{first_name}]!")
         except AttributeError:
             print("Ошибка! Получено событие из сообщений сообщества. Ожидалось из беседы")
             sleep(5)
             continue
+
+
+def main():
+    sending_time = generate_sending_time()
+    user_id = get_random_user()
+    is_generated = True
+    while True:
+        if longpoll.check():  # Если есть события
+            event_loop()
+        elif is_generated and sending_time == datetime.now().strftime("%H:%M"):
+            send_hello_to_user(user_id)
+            is_generated = False
+        elif not is_generated:
+            user_id = get_random_user()
+            sending_time = generate_sending_time()
+            is_generated = True
 
 
 if __name__ == "__main__":
---
+++ timer.py
@@ -0,0 +1,3 @@
+class Timer:
+    def __init__(self):
+        self.sending_time = ""
